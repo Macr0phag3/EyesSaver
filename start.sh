9e89ee8910b55d0ea3ad3e96cc04be7228d899f1
Macr0phag3 <997334636@qq.com>
2019-04-04 17:12:56 +0800

    upload latest code

--- start.sh
+++ start.sh
@@ -15,9 +15,7 @@ if [[ $delta -gt 60 ]]; then
     thetime=`echo $(($delta/60))|awk '{printf("%.1f", $1)}'`
 fi
 
-if [ "$delta" -ge $[3*60] ];then # 锁屏超过 3 分钟，说明是在休息，否则视为临时有事
-    # 休息结束，重置工作计时器
-    date +%s > $saverpath/resttimer
+if [ "$delta" -ge $[10] ];then # 锁屏超过 3 分钟，说明是在休息，否则视为临时有事
 
     # 停止 sleep，弹出工作提醒
     isrested=$(ps -ef|grep "sleep [0-9]*eyesaver")
@@ -26,8 +24,12 @@ if [ "$delta" -ge $[3*60] ];then # 锁屏超过 3 分钟，说明是在休息，
         if [ "$result" == '好的 ☺️' ]; then
             echo $logtime "提前结束休息" >> $saverpath/log
             ps -ef|grep "sleep [0-9]*eyesaver"| awk '{print $2}'| xargs kill -9
+            # 休息结束，重置工作计时器
+            date +%s > $saverpath/resttimer
         fi
+
     else
+        date +%s > $saverpath/resttimer
         echo $logtime "检测到解锁事件，休息时长 $thetime $unit""，休息结束，开始工作，重置工作计时器" >> $saverpath/log
     fi
 else
